5c147e1e09a4dd32ddc56fc7f1b2ebd007efbbb9
Mike LaFreniere <mlafreniere17@hotmail.com>
2022-10-12 17:35:42 -0600

    replace handlers w/ async/await

--- src/index.ts
+++ src/index.ts
@@ -11,64 +11,77 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Endpoints
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const newUser = new UserModel(req.body);
-  newUser
-    .save()
-    .then((user) => res.status(201).send(user.toJSON()))
-    .catch((err) => res.status(400).send(err));
+  try {
+    const user = await newUser.save();
+    res.status(201).send(user.toJSON());
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
-app.get('/users', (req, res) => {
-  UserModel.find({})
-    .then((users) => res.status(200).send(users))
-    .catch(() => res.status(500).send('Unable to list users'));
+app.get('/users', async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.status(200).send(users);
+  } catch (err) {
+    res.status(500).send('Unable to list users');
+  }
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const _id = req.params.id;
   if (!mongoose.Types.ObjectId.isValid(_id)) {
-    res.status(400).send();
-    return;
+    return res.status(400).send();
   }
-  UserModel.findById(_id)
-    .then((user) => {
-      if (!user) {
-        res.status(404).send();
-      }
-      res.status(200).send(user);
-    })
-    .catch((err) => res.status(500).send(err));
+
+  try {
+    const user = await UserModel.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const newTask = new TaskModel(req.body);
-  newTask
-    .save()
-    .then((task) => res.status(201).send(task.toJSON()))
-    .catch((err) => res.status(400).send(err));
+  try {
+    const task = await newTask.save();
+    res.status(201).send(task.toJSON());
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
-app.get('/tasks', (req, res) => {
-  TaskModel.find({})
-    .then((tasks) => res.status(200).send(tasks))
-    .catch(() => res.status(500).send('Unable to list tasks'));
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await TaskModel.find({});
+    res.status(200).send(tasks);
+  } catch (err) {
+    res.status(500).send('Unable to list tasks');
+  }
 });
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   const _id = req.params.id;
   if (!mongoose.Types.ObjectId.isValid(_id)) {
     res.status(400).send();
     return;
   }
-  TaskModel.findById(_id)
-    .then((task) => {
-      if (!task) {
-        res.status(404).send();
-      }
-      res.status(200).send(task);
-    })
-    .catch((err) => res.status(500).send(err));
+
+  try {
+    const task = await TaskModel.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.status(200).send(task);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 app.listen(port, () => console.log('Server is running on port ' + port));
